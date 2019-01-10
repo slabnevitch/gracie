9a19d86bf214d7f73b7b0338dce9000d5cb576b8
slabnevitch <handofschpilrain@mail.ru>
2019-01-10 18:30:36 +0500

    reload

--- js/common.js
+++ js/common.js
@@ -1,6 +1,35 @@
 $(function() {
 
-	// slick
+	$(document).ready(function() {
+		// magnific-popup
+		$('.card-slider__popup').magnificPopup({
+				// delegate: 'a',
+				type: 'image',
+				closeOnContentClick: false,
+				closeBtnInside: false,
+				mainClass: 'mfp-with-zoom mfp-img-mobile',
+				image: {
+					verticalFit: true,
+					// titleSrc: function(item) {
+					// 	return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+					// }
+				},
+				gallery: {
+					enabled: true
+				},
+				zoom: {
+					enabled: true,
+				duration: 300, // don't foget to change the duration also in CSS
+				opener: function(element) {
+					return element.find('img');
+				}
+			}
+			
+		});
+
+		// end magnific-popup
+
+		// slick
 		var $mainSlider = $('.main-slider').slick({
 			fade: true,
 			dots: true,
@@ -43,7 +72,8 @@ $(function() {
 
 		$('.card-slider__display').slick({
 			fade: true,
-			arrows: false
+			arrows: false,
+			adaptiveHeight: true
 		});
 
 		$('.card-slider__nav').slick({
@@ -56,6 +86,9 @@ $(function() {
   			focusOnSelect: true
 		});
 	// end of slick
+	});
+
+	
 
 	// cart-counter
 		$('.card-counter__button').click(function(e) {
